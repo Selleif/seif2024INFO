c7b945c8426e6dbc57cb42a52783c310769b4731
Selleif <seif072017@gmail.com>
2024-10-17 10:04:56 +0000

    done

---
+++ INF/Esercizio1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    FILE* testo;
+     char ch;
+
+    testo = fopen("text.txt", "r");
+    if(testo != NULL){
+        printf("apertura file riuscita :)\n");
+    }else {
+        printf("apertura file non riusicta :(\n");
+        return 1;
+    }
+
+    while(!feof(testo)){
+        ch = fgetc(testo);
+        if(ch != EOF){
+            printf("%c", ch);
+        }
+    }
+
+    fclose(testo);
+    return 0;
+}
